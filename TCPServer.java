044735c85d2642fa7ab65da6b162d2674b992fc8
Nguyen P. Do <dophucnguyen2005@gmail.com>
2026-03-16 10:05:53 -0500

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -1,31 +1,77 @@
 import java.io.*;
 import java.net.*;
+import java.util.Date;
+
+class ClientHandler implements Runnable{
+    private Socket socket;
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        try{
+            BufferedReader clientInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream serverOutput = new DataOutputStream(socket.getOutputStream());
+
+            String clientName = clientInput.readLine();
+            System.out.println("Client " + clientName + " connected.");
+            serverOutput.writeBytes("Welcome client " + clientName + "\n");
+
+            String clientEquation;
+            while ((clientEquation = clientInput.readLine()) != null){
+                if (clientEquation.equalsIgnoreCase("Exit")){
+                    break;
+                }
+                System.out.println("Request from client " + clientName + ": " + clientEquation);
+            
+                try {
+                    String[] parts = clientEquation.split(" ");
+                    double num1 = Double.parseDouble(parts[0]);
+                    double num2 = Double.parseDouble(parts[2]);
+                    String operator = parts[1];
+                    double result = 0;
+
+                    if (operator.equals("+")){
+                        result = num1 + num2;
+                    }
+                    else if (operator.equals("-")){
+                        result = num1 - num2;
+                    }
+                    else if (operator.equals("*")){
+                        result = num1 * num2;
+                    }
+                    else if (operator.equals("/")){
+                        result = num1 / num2;
+                    }
+
+                    serverOutput.writeBytes("Result: " + result + "\n");
+                } catch (Exception e){
+                    serverOutput.writeBytes("Error: Bad format. Must write '1 + 1'\n");
+                }
+            }
+
+            System.out.println("LOG: " + clientName + " disconnected.");
+            socket.close();
+        } catch (IOException e){
+            System.out.print("Error handling client/");
+        }
+    }
+}
 
 class TCPServer {
 
   public static void main(String argv[]) throws Exception
     {
-      String clientSentence;
-      String capitalizedSentence;
-
       ServerSocket welcomeSocket = new ServerSocket(6789);
 
       while(true) {
-
-            System.out.println("Server is UP and running!");
             Socket connectionSocket = welcomeSocket.accept();
-
-           BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
-           DataOutputStream  outToClient =
-             new DataOutputStream(connectionSocket.getOutputStream());
-
-           clientSentence = inFromClient.readLine();
-
-            System.out.println("Server received message!" + clientSentence);
-           capitalizedSentence = clientSentence.toUpperCase() + '\n';
-
-           outToClient.writeBytes(capitalizedSentence);
-        }
+            ClientHandler handler = new ClientHandler(connectionSocket);
+            Thread client = new Thread(handler);
+            client.start();
+            welcomeSocket.close();
+      }
     }
 }
+
